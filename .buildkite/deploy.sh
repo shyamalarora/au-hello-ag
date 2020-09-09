e5300d4e48289a1990e59adfdd55c3e174be00aa
shyamalarora <arora.shyamal@gmail.com>
2020-09-09 15:45:34 +1000

    Testing buildkite pipeline

---
+++ .buildkite/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+COMMAND=${1}
+FLAGS=""
+
+if [ -z ${DEBUG+x} ]
+then 
+    FLAGS="${FLAGS} -v"
+else 
+    FLAGS="${FLAGS} -vvvvv"
+fi
+
+echo "Flags"
+echo "${FLAGS}"
+
+docker-compose --log-level ERROR run --rm ${COMMAND}
